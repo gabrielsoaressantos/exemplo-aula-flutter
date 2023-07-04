66f847dbbcbd389bbec6aa4ddbf4829540eb61e3
gabriel <mail.gabrielsoares@gmail.com>
2023-07-04 17:41:54 -0300

    add ContatoDAO

---
+++ lib/data/contato_dao.dart
@@ -0,0 +1,17 @@
+import '../domain/contato.dart';
+
+List<Contato> _contatos = [];
+
+class ContatoDAO {
+  void criar(Contato contato) {
+    _contatos.add(contato);
+  }
+
+  List<Contato> buscarTodos() {
+    return _contatos;
+  }
+
+  void deletar(Contato contato) {
+    _contatos.remove(contato);
+  }
+}
